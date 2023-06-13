3b8e1d9cd45d0cd1b888c61e0a1704242e57d3eb
nodedev74 <nodedev74@gmail.com>
2023-06-13 17:31:13 +0200

    Applicatiuon commented

--- src/main/java/com/github/nodedev74/jfbx/application/AppilcationInterface.java
+++ src/main/java/com/github/nodedev74/jfbx/application/AppilcationInterface.java
@@ -1,9 +1,19 @@
 package com.github.nodedev74.jfbx.application;
 
+/**
+ * Interface that provides methods to interact with.
+ */
 public interface AppilcationInterface {
 
+    /**
+     * Runs at application start.
+     */
     public void start();
 
+    /**
+     * Runs at application stop.
+     * By default there will be no stop action.
+     */
     public default void stop() {
     }
 }
--- src/main/java/com/github/nodedev74/jfbx/application/Application.java
+++ src/main/java/com/github/nodedev74/jfbx/application/Application.java
@@ -7,12 +7,26 @@ import com.github.nodedev74.jfbx.NativeLoader;
 import com.github.nodedev74.jfbx.stage.Stage;
 import com.github.nodedev74.jfbx.stage.control.Control;
 
+/**
+ * 
+ * Abstract class representing an application.
+ * Provides methods for launching and managing the application lifecycle.
+ */
 public abstract class Application implements AppilcationInterface {
+
+    protected static long FPS_TARGET = 30;
 
     private static boolean isRunning = true;
 
     public static Stage currentStage;
 
+    /**
+     * Launches the specified application class.
+     * Loads the Vulkan library, creates a stage, and starts the application
+     * lifecycle.
+     * 
+     * @param app the application class to launch
+     */
     public static void launch(Class<? extends Application> app) {
         try {
             NativeLoader.load("libvulkan");
@@ -27,35 +41,51 @@ public abstract class Application implements AppilcationInterface {
         }
     }
 
+    /**
+     * Exits the application.
+     * Sets the isRunning flag to false.
+     */
     public static void exit() {
         isRunning = false;
     }
 
+    /**
+     * Manages the application lifecycle.
+     * Continuously processes the lifecycle of active controls in the current stage.
+     * Exits the application when there are no more active controls.
+     */
     private static void lifecycle() {
+        long frameTime = 1000 / FPS_TARGET;
+        long startTime = System.currentTimeMillis();
+
         while (isRunning) {
             ArrayList<? super Control> children = currentStage.children;
-            if (children.size() != 0) {
+            if (!children.isEmpty()) {
                 Iterator<? super Control> iterator = children.iterator();
                 while (iterator.hasNext()) {
-                    Object child = iterator.next();
-                    Control element = (Control) child;
+                    Control element = (Control) iterator.next();
                     if (element.isActive()) {
                         element.lifecycle();
                     } else {
                         iterator.remove();
-                        if (children.size() == 0) {
-                            Application.exit();
-                        }
                     }
                 }
             } else {
                 Application.exit();
             }
-            try {
-                Thread.sleep(34);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            long sleepTime = frameTime - elapsedTime;
+
+            if (sleepTime > 0) {
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+
+            startTime = System.currentTimeMillis();
         }
     }
 
